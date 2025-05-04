e6881a788584152714c959ea8ff541bf61f70cf7
Rafael <rafael.pinheiro@dmslog.com>
2025-05-04 02:12:03 -0300

    feat: remove gender of visitor register

--- platform/website/forms/visitor.py
+++ platform/website/forms/visitor.py
@@ -5,14 +5,11 @@ from ..models.visitor import Visitor
 class VisitorForm(forms.ModelForm):
     class Meta:
         model = Visitor
-        fields = ['name', 'gender', 'phone', 'address', 'neighborhood', 'prayer_request']
+        fields = ['name', 'phone', 'address', 'neighborhood', 'prayer_request']
         widgets = {
             'name': forms.TextInput(attrs={
                 'class': 'mt-1 block w-full rounded-xl border border-gray-300 shadow-sm p-2 focus:ring-[var(--color-primary)] focus:border-[var(--color-primary)]',
                 'placeholder': 'Nome completo'
-            }),
-            'gender': forms.Select(attrs={
-                'class': 'mt-1 block w-full rounded-xl border border-gray-300 shadow-sm p-2 text-gray-700 bg-white focus:ring-[var(--color-primary)] focus:border-[var(--color-primary)]'
             }),
             'phone': forms.TextInput(attrs={
                 'id': 'id_telephone',
