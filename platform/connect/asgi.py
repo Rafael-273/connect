92612506b531a20fd52b1ae56da65b21d5675359
Rafael <rafael.pinheiro@dmslog.com>
2025-07-01 00:04:00 -0300

    feat: create transcription service

--- platform/connect/asgi.py
+++ platform/connect/asgi.py
@@ -1,12 +1,10 @@
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import website.routing
+import website.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connect.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
