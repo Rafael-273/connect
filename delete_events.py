e2522d17db31a482eb02971a537cae5ca66deb63
Rafael Pinheiro <rafael.pinheiro@dmslog.com>
2025-09-17 14:06:48 -0300

    fix: remove extra files

--- delete_events.py
+++
@@ -1,7 +0,0 @@
-from website.models.event import Event
-from safedelete.models import HARD_DELETE
-
-# Limpar eventos recorrentes existentes
-print('Deletando eventos recorrentes existentes...')
-deleted_count = Event.objects.filter(is_recurring=True).delete(force_policy=HARD_DELETE)
-print(f'Eventos deletados: {deleted_count}')
