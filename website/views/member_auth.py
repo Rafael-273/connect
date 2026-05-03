f040c8d3e60c117ddd06fc60482affd23f4164a6
Rafael Pinheiro <rafael.pinheiro@dmslog.com>
2026-05-03 16:44:26 -0300

    feat: adjustments roteiro

--- website/views/member_auth.py
+++ website/views/member_auth.py
@@ -68,6 +68,7 @@ class MemberDashboardView(MemberRequiredMixin, MinistrationContextMixin, View):
             'is_approver': flags['is_approver'],
             'is_ministration_member': flags['is_ministration'],
             'is_boas_vindas_member': flags['is_boas_vindas'],
+            'is_moderacao_member': flags['is_moderacao'],
             'member_schedules': member_schedules,
             'is_new_member': not any([flags['has_ministries'], flags['is_approver'], flags['can_consolidate']]),
         }
@@ -82,6 +83,11 @@ class MemberDashboardView(MemberRequiredMixin, MinistrationContextMixin, View):
                 member=member,
                 is_active=True,
                 ministry__name__icontains='boas vindas',
+            ).exists(),
+            'is_moderacao': MinistryMembership.objects.filter(
+                member=member,
+                is_active=True,
+                ministry__name__icontains='modera',
             ).exists(),
         }
 
